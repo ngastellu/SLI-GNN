131b7a65b32a904bc00dfbd8bc46d917125df589
Nico <nicolas.gastellu@mail.mcgill.ca>
2025-02-03 11:26:49 -0500

    Switched back to single target property

--- data/csv_from_db.py
+++ data/csv_from_db.py
@@ -9,9 +9,15 @@ db_path = f'/Users/nico/Desktop/scripts/OPVGCN/data/{db_name}.db'
 
 db = connect(db_path)
 
-out_csv = f'dataset/targets/{db_name}-targets.csv'
 
-with open(out_csv,'w',newline='') as csvfile:
-    kerouac = csv.writer(csvfile)
-    for row in db.select():
-        kerouac.writerow([row.id, row.data['HOMO'], row.data['LUMO'], row.data['ET1'], row.data['DH'],row.data['DL']])
+molprops = ['HOMO', 'LUMO', 'ET1', 'DH', 'DL']
+
+for mp in molprops:
+    print(f'Writing {mp} CSV...')
+    out_csv = f'dataset/targets/{db_name}_{mp}-targets.csv'
+    with open(out_csv,'w',newline='') as csvfile:
+        kerouac = csv.writer(csvfile)
+        for row in db.select():
+            kerouac.writerow([row.id, row.data[mp]])
+
+        # kerouac.writerow([row.id, row.data['HOMO'], row.data['LUMO'], row.data['ET1'], row.data['DH'],row.data['DL']])
